9009f2d831b85128b1efb1d944e2bcd45f2b5b68
Anmol Pandey <Anmol.Pandey@Coforge.com>
2026-03-06 14:46:09 +0530

    hello commit

---
+++ helloapplication/src/test/java/helloprg/helloapplication/AppTest.java
@@ -0,0 +1,19 @@
+package helloprg.helloapplication;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
